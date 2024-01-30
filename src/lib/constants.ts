d74b12628f7167e0938408f160b8b03042ff7d0d
nna-n5Ember13 <namanh200313@gmail.com>
2024-01-30 18:27:58 +0700

    🐳 chore: add constants value

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,3 +8,24 @@ export enum Tech {
   TYPESCRIPT = 'typescript',
   NESTJS = 'nestjs'
 }
+
+type ProjectItem = {
+  title: string
+  description: string
+  image: string
+}
+
+export const projectItems: ProjectItem[] = [
+  {
+    title: 'Aerocamp',
+    description:
+      'AeroCamp is where people can join and choose favorite campgrounds for their trip, or can add your own campgrounds, write review',
+    image: 'project1.png'
+  },
+  {
+    title: 'GDSC HANU Platform',
+    description:
+      'GDSC HANU represented Hanoi University in Google Developer Student Club Community. We focus on Software Engineering and develop tech community.',
+    image: 'gdsc_platform.jpg'
+  }
+]
