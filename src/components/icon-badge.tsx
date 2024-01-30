3fe9651b1d1da6a034910921970a9dd6852d8dfa
nna-n5Ember13 <namanh200313@gmail.com>
2024-01-30 18:29:55 +0700

    🌈 style: minor style change

--- src/components/icon-badge.tsx
+++ src/components/icon-badge.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 function IconBadge(props: Props) {
   return (
-    <div className='flex items-center mr-2 bg-blue-400 py-1 px-2 rounded-md'>
+    <div className='flex items-center bg-blue-400 py-1 px-2 rounded-md'>
       {props.icon}
       <p className='ml-1 md:text-14'>{props.description}</p>
     </div>
