722059effb57f77a0eb4b1ac344524e485d5b43f
nna-n5Ember13 <namanh200313@gmail.com>
2024-01-30 18:46:26 +0700

    ✨ feat: link to projects

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -2,23 +2,31 @@ import { projectItems } from '@/lib/constants'
 import Card from './card-container'
 import SectionWrapper from './section-wrapper'
 import React from 'react'
+import Link from 'next/link'
 
 function Projects() {
   return (
     <SectionWrapper title='Projects' id='project'>
       <div className='grid md:grid-cols-2 md:gap-10 gap-4 mt-8 w-full'>
-        <Card
-          title={projectItems[0].title}
-          description={projectItems[0].description}
-          imageUrl={projectItems[0].image}
-          techstack={['reactjs', 'expressjs', 'mongodb', 'expressjs']}
-        />
-        <Card
-          title={projectItems[1].title}
-          description={projectItems[1].description}
-          imageUrl={projectItems[1].image}
-          techstack={['nuxtjs', 'sass', 'typescript']}
-        />
+        <Link
+          href='https://github.com/ng-namanh/aerocamp'
+          className='cursor-pointer'
+        >
+          <Card
+            title={projectItems[0].title}
+            description={projectItems[0].description}
+            imageUrl={projectItems[0].image}
+            techstack={['reactjs', 'expressjs', 'mongodb', 'expressjs']}
+          />
+        </Link>
+        <Link href='https://gdschanu.com/'>
+          <Card
+            title={projectItems[1].title}
+            description={projectItems[1].description}
+            imageUrl={projectItems[1].image}
+            techstack={['nuxtjs', 'sass', 'typescript']}
+          />
+        </Link>
       </div>
     </SectionWrapper>
   )
